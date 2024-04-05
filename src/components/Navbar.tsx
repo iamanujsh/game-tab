9aa8aea0ec64cddb4dc874867db858db515a5c20
Anuj Sharma <cloudu07@gmail.com>
2024-04-05 14:15:15 +0530

    implement dark mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import logo from "../assets/movietab.png";
+import { ModeToggle } from "./ModeToggle";
 
 const Navbar = () => {
   return (
-    <div className="flex p-3 items-center">
+    <div className="flex p-3 items-center justify-between">
       <img width={70} height={70} src={logo} alt="logo" />
       <p className="font-semibold text-xl">Navbar</p>
+      <ModeToggle />
     </div>
   );
 };
